f85925b4be6ace7d3a7a1124b19a0b5fbdaecdaf
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-07-06 07:43:15 +0530

    Started with section 1- completed simple interest

---
+++ src/Section_1_Basics/One.java
@@ -0,0 +1,31 @@
+package Section_1_Basics;
+//Wap to calculate simple interest
+import java.util.Scanner;
+
+public class One {
+    private static Scanner scn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        getInputs();
+    }
+
+    private static void getInputs(){
+        //takes inputs from the user
+        System.out.println("Enter Principle: ");
+        double p = scn.nextDouble();
+
+        System.out.println("Enter Rate: ");
+        float r = scn.nextFloat();
+
+        System.out.println("Enter Time: ");
+        int t = scn.nextInt();
+
+        calcSimpleInterest(p,r,t);
+    }
+
+    public static void calcSimpleInterest(double p,float r, int t ){
+        //calculate simple interest
+        double si = (p*r*t)/100;
+        System.out.println("Simple Interest: "+si);
+    }
+}
