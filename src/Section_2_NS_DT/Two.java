c16d6cdc3c463d6ca55e4e21ff4d74667673ea72
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-07-11 14:46:20 +0530

    Section2: Two Added

---
+++ src/Section_2_NS_DT/Two.java
@@ -0,0 +1,12 @@
+package Section_2_NS_DT;
+
+public class Two {
+    public static void main(String[] args) {
+        int c;
+        for(int i = 0; i<=300;i+=20){
+            c = (5*(i-32))/9;
+            System.out.println(i+"\t"+c);
+
+        }
+    }
+}
